326aadeff1842c836cbe9f2ccfb8b4974969d55a
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-04-14 04:37:05 +0100

    join! macro works like await but does it on more that one future

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use futures::executor::block_on;
 
 fn main() {
-    let song = block_on(learn_song());
-    block_on(sing_song(song));
-    block_on(dance());
+    block_on(async_main());
 }
 
 struct Song {
@@ -27,4 +25,23 @@ async fn sing_song(song: Song) {
 
 async fn dance() {
     println!("I am dancing right now");
+}
+
+async fn learn_and_sing() {
+    // using await keyword
+    let song = learn_song().await; // returns a value
+    sing_song(song).await;
+}
+
+async fn async_main() {
+    let future_one = learn_and_sing(); // returns a future
+    let future_two = dance();
+
+    // `join!` is like `.await` but can wait for multiple futures concurrently.
+    // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
+    // future will take over the current thread. If `dance` becomes blocked,
+    // `learn_and_sing` can take back over. If both futures are blocked, then
+    // `async_main` is blocked and will yield to the executor.
+
+    futures::join!(future_one, future_two); // simontaneously poll the futures
 }
